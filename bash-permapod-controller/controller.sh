bac240634de6d4d3ad3ac17702c0a1c133ce9094
celso.rodrigues <celso.cunha.rodrigues@gmail.com>
2023-03-31 03:16:06 +0200

    Run both controller watchers in background

--- bash-permapod-controller/controller.sh
+++ bash-permapod-controller/controller.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-echo "Controller starting"
+k=$(which kubectl)
+current_user=$(whoami)
+
+echo "cleanup all kubectl(s) running in the background for user ${current_user}"
+for process in $(ps -eo user,pid,pgid,tgid,args| awk 'NR == 1 || ($3 != -1 && $2 != $3)' |grep kubectl|awk -v user=${current_user} '{if ($1 == user) {print $2}}'); do kill -9 ${process} > /dev/null 2>&1 ; done
 
 reconcile () {
-    ns=$1
-    echo "Reconciling Namespace: $ns"
+  local ns=${1}
+  local k=$(which kubectl)
 
-    kubectl get pod -n $ns permapod 2>&1 > /dev/null
-
-    return_value=$?
-
-    if [ $return_value -eq 1 ]; then
-        echo "Permapod does not exist, creating pod"
-        kubectl run permapod -n $ns --image ubuntu -- sleep 1000000
-    else
-        echo "Permapod EXISTS, doing nothing"
-    fi
+  echo "reconciling namespace: ${ns}"
+  ${k} get po alpine -n ${ns} > /dev/null 2>&1
+  local exit_code=${?}
+  if [[ ${exit_code} == 1 ]]; then
+    echo "pod not found in ns ${ns}... CREATING"
+    ${k} -n ${ns} run alpine --image=alpine:latest -- sleep 10000000
+  else
+    echo "pod found in ns ${ns}... DO NOTHING"
+  fi
 }
 
-# The function needs to be exported so that is can be called with xargs.
 export -f reconcile
 
-kubectl get namespaces -w -o=jsonpath='{.metadata.name}{"\n"}' | xargs -L1 bash -c 'reconcile "$@"' _
+echo "Starting controller in the background..."
+echo -e "\nView controller logs with: \ntail -F /tmp/controller.log\n"
+
+${k} get ns -w -ojsonpath='{.metadata.name}{"\n"}'|xargs -L1 bash -c 'reconcile "${@}" >> /tmp/controller.log & ' _ > /dev/null 2>&1 &
+${k} get po -A -w -ojsonpath='{.metadata.namespace}{"\n"}'|xargs -L1 bash -c 'reconcile "${@}" >> /tmp/controller.log & ' _ > /dev/null 2>&1 &
+
