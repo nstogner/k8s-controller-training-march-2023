c8e1e91802e36897eb695538bfece7f3e1f49a6e
Nick Stogner <nicholas.stogner@gmail.com>
2023-04-05 15:47:52 -0400

    Session 4

---
+++ go-load-test-controller/api/v1/loadtest_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// LoadTestSpec defines the desired state of LoadTest
+type LoadTestSpec struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Address  string `json:"address"`
+	Method   string `json:"method"`
+	Duration string `json:"duration"`
+}
+
+// LoadTestStatus defines the observed state of LoadTest
+type LoadTestStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// LoadTest is the Schema for the loadtests API
+type LoadTest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LoadTestSpec   `json:"spec,omitempty"`
+	Status LoadTestStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// LoadTestList contains a list of LoadTest
+type LoadTestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LoadTest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LoadTest{}, &LoadTestList{})
+}
